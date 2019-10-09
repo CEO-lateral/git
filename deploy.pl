14a481767b3b0e578d0fce89fd59265dbb6dfbf4
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-09 11:36:07 +0300

    thirteen commit

--- deploy.pl
+++ deploy.pl
@@ -5,3 +5,4 @@ cd /var/www/domains/k.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
 git pull > /dev/null
 #git@bitbucket.org:gafurovstudio/g2.git
+chmod -R 755 /var/www/domains/k.qzo.su > /dev/null
