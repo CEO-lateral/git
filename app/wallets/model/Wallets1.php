a903775a15d96e555b31f6f945dce5e033a84171
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-11 13:06:32 +0300

    26-th commit

--- app/wallets/model/Wallets1.php
+++ app/wallets/model/Wallets1.php
@@ -5,15 +5,14 @@
  * $this->db is a PDO object
  * Has a config in /core/config/database.php
  */
-
+ 
 class Wallets1 extends Model{
-
-	//авторизация пользователя
-    public function getName(){	
-	$array = array("My", "name", "is", "Kostya");
-	$array1=Model::table("wallets")->get(array("id", "id_user","amount","image"))->send();
-		$this->viewJSON($array1);
 	
-		}
+    public function getName(){	
+		//$array1=Model::table("wallets")->get(array("id", "id_user","amount","image"))->send();
+		//$this->viewJSON($array1);
+		echo ("id=1");
+		echo ("amount=4");
+	}
 }
 ?>
