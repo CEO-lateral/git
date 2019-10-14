afdb569aba2b7da6ab0567628db47184a9444fc6
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-14 11:08:57 +0300

    29-th commit

--- app/wallets/PublicWalletsController.php
+++ app/wallets/PublicWalletsController.php
@@ -13,6 +13,18 @@ class PublicWalletsController {
 $my_name=new Wallets1();
       $my_name->getName();
 	}
+	public function addwallets(){
+		if(isset($_GET['id']))
+{   
+    $link = mysqli_connect($host, $user, $password, $database) 
+            or die("Ошибка " . mysqli_error($link)); 
+    $id = mysqli_real_escape_string($link, $_GET['id']);
+    $query ="DELETE FROM wallets WHERE id = '5'";
+ 
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+    mysqli_close($link);
+		
+	}
 
 }
 
