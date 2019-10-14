95add5e55c49a9cc3437dd08d89b511c51b5dd85
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-14 14:09:30 +0300

    44-th commit

--- app/wallets/model/Wallets1.php
+++ app/wallets/model/Wallets1.php
@@ -43,7 +43,7 @@ public function editTransfers(){
 }
 public function pluswallets(){
 	$array1=Model::table("wallets")->get(array("id","amount"))->filter(array("id" => 1))->send();
-	$array2="amount" => 5000;
+	$array2=amount[0];
 	var_dump($array2);
 	//var_dump($array1);
 	//$array2=id["1"] + $_GET["amount"];
