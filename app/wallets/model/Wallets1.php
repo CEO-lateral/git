79e983dfe58620df8d0d32da1e686c70cb5c1cc8
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-14 11:05:04 +0300

    28-th commit

--- app/wallets/model/Wallets1.php
+++ app/wallets/model/Wallets1.php
@@ -13,5 +13,26 @@ class Wallets1 extends Model{
 		//$this->viewJSON($array1);
 		 echo ($_GET["id"]);
 		 echo ($_GET["amount"]);
-}}
+}
+public function addwallets(){
+if(isset($_GET['id']))
+{   
+    $link = mysqli_connect($host, $user, $password, $database) 
+            or die("Ошибка " . mysqli_error($link)); 
+    $id = mysqli_real_escape_string($link, $_GET['id']);
+    $query ="DELETE FROM wallets WHERE id = '5'";
+ 
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+    mysqli_close($link);
+}
+}
+	
+	
+}
+
+
+
+
+
+}
 ?>
