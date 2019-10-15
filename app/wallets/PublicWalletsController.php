4e9e80aac8c1b9ff843d7023d67e09ff18106aa5
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-15 13:04:52 +0300

    55-th commit

--- app/wallets/PublicWalletsController.php
+++ app/wallets/PublicWalletsController.php
@@ -1,14 +1,5 @@
 <?php
-/*
- * Every class derriving from Model has access to $this->db
- * $this->db is a PDO object
- * Has a config in /core/config/database.php
- */
 class PublicWalletsController {
-   /* public function getExample(){
-$my_name=new Wallets1();
-      $my_name->getName();
-	}*/
 	public function delwallets(){
 		$mywallet=new Wallets1();
 		$mywallet->delwallets();	
