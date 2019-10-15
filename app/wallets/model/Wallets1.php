1fdafb2e7c4d83d095efe8419c86b421fe24472c
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-15 12:48:01 +0300

    54-th commit

--- app/wallets/model/Wallets1.php
+++ app/wallets/model/Wallets1.php
@@ -56,10 +56,10 @@ public function minuswallets(){
 	
 }
 public function requestwallets(){
-$stmt = self::$db->prepare("SELECT * FROM  `wallets`,`wallets_transfers` WHERE wallets.id= :id");
-$result_query = $stmt->execute(array(":id" => self::$params_url['id']));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-$this->viewJSON($rows);	
+	$stmt = self::$db->prepare("SELECT * FROM  `wallets`,`wallets_transfers` WHERE wallets.id= :id");
+	$result_query = $stmt->execute(array(":id" => self::$params_url['id']));
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+	$this->viewJSON($rows);	
 	
 }
 
