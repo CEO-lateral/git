8853841049a32191c32a7e46a5332e6e49b0fd6e
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-15 13:53:41 +0300

    57

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -61,11 +61,10 @@ class Wallets extends Model{
 		}
 	}
 	public function requestwallets(){
-			$stmt = self::$db->prepare("SELECT * FROM  `wallets`,`wallets_transfers` WHERE wallets.id= :id");
-			$result_query = $stmt->execute(array(":id" => self::$params_url['id']));
-			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-			$this->viewJSON($rows);		
-		}
+		$stmt = self::$db->prepare("SELECT * FROM  `wallets`,`wallets_transfers` WHERE wallets.id= :id");
+		$result_query = $stmt->execute(array(":id" => self::$params_url['id']));
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+		$this->viewJSON($rows);				
 	}
 
 }		
