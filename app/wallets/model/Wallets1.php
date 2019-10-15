807e23842726ba34ab819fa45d737395e3c5ec3b
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-15 12:18:37 +0300

    51-th commit

--- app/wallets/model/Wallets1.php
+++ app/wallets/model/Wallets1.php
@@ -48,7 +48,7 @@ public function pluswallets(){
     $array2=Model::table("wallets")->edit(array("amount" => $i),array("id" => 1))->send();
 	
 }
-public function pluswallets(){
+public function minuswallets(){
 	$array1=Model::table("wallets")->get(array("id","amount"))->filter(array("id" => 1))->send();
 	$i=$array1[0]["amount"] - $_GET["amount"];
 	echo($i);
