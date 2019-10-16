699b3a55e89227d2c0173c68d21b4ef5dbeeec82
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-16 13:05:45 +0300

    75-th

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -72,7 +72,7 @@ class Wallets extends Model{
 		if((!empty($_GET['amount'])) && (!empty($_GET['id']))){
 			$array1=Model::table("wallets")->get(array("id","amount"))->filter(array("id" => $_GET['id']))->send();
 			$i=$array1[0]["amount"] - $_GET["amount"];
-			if($i>0){
+			if($i>=0){
 			echo($i);
 			$array2=Model::table("wallets")->edit(array("amount" => $i),array("id" => $_GET['id']))->send();
 			}
