06f9f4fc8d23aab5b65ee1cd7fa8433cdc6508aa
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-16 12:11:16 +0300

    70-th

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -60,7 +60,7 @@ class Wallets extends Model{
 	public function plusWallets(){
 		if((!empty($_GET['amount'])) && (!empty($_GET['id']))){
 			$array1=Model::table("wallets")->get(array("id","amount"))->send();
-			$i=$array1[0]["amount"] + $_GET["amount"];
+			$i=$array1[$_GET['id']]["amount"] + $_GET["amount"];
 			echo($i);
 			$array2=Model::table("wallets")->edit(array("amount" => $i),array("id" => $_GET['id']))->send();
 		}
