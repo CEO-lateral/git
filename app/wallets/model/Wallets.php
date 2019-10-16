eb61ec063e8549672d0e44f3e09cf5983ace3940
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-16 12:34:25 +0300

    71-th

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -69,12 +69,10 @@ class Wallets extends Model{
 			}
 	}
 	public function minusWallets(){
-		if((!empty($_GET['amount'])) && (!empty($_GET['id']))){
+		if((!empty($_GET['amount'])) && (!empty($_GET['id'])) && (amount>0)){
 			$array1=Model::table("wallets")->get(array("id","amount"))->filter(array("id" => $_GET['id']))->send();
-			if(("amount">0)){
 			$i=$array1[0]["amount"] - $_GET["amount"];
 			echo($i);
-			}
 			$array2=Model::table("wallets")->edit(array("amount" => $i),array("id" => $_GET['id']))->send();
 		}
 			else{
