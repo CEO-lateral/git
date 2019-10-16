cec91308017ba4400288187a2a434d8ca9a62434
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-16 14:10:17 +0300

    79-th

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -98,7 +98,7 @@ class Wallets extends Model{
 	public function request1Wallets(){
 		if((!empty($_GET['id_owner'])) && (!empty($_GET['id_wallet'])) && (!empty($_GET['rights']))){
 		$stmt = self::$db->prepare(" INSERT INTO wallets_owners (id_owner,id_wallet,rights) VALUES (4,4,'non-active')");
-			$result_query = $stmt->execute(array(":id_owner" => self::$params_url['id_owner']),array(":id_wallet" => self::$params_url['id_wallet']),array(":rights" => self::$params_url['rights']));
+			$result_query = $stmt->execute(array(":id_owner" => self::$params_url['id_owner'],":id_wallet" => self::$params_url['id_wallet'],":rights" => self::$params_url['rights']));
 			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 		}
 		else{
