8accdbea2d4f507ff368224fdbde94a53d3da6f7
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-17 13:07:18 +0300

    81

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -18,6 +18,7 @@ class Wallets extends Model{
 	public function addWallets(){
 		if(!empty($_GET['id_user']) && (!empty($_GET['amount']) || empty($_GET['amount'])) && !empty($_GET['image'])){ 
 			$array1=Model::table("wallets")->get(array("id"))->filter(array("id" => $_GET["id_user"] ))->send();
+			echo($array1);
 				if(empty($array1[0]["id"])){
 					$array1=Model::table("wallets")->add(array("id_user" => $_GET["id_user"], "amount" => $_GET["amount"],"image" => $_GET["image"]))->send();
 					var_dump($array1[0]["id"]);
