fea631c0aa61e3572834443f51350358773e4824
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-17 12:56:55 +0300

    81

--- app/wallets/model/Wallets.php
+++ app/wallets/model/Wallets.php
@@ -17,10 +17,10 @@ class Wallets extends Model{
 	}	
 	public function addWallets(){
 		if(!empty($_GET['id_user']) && (!empty($_GET['amount']) || empty($_GET['amount'])) && !empty($_GET['image'])){ 
-			$array1=Model::table("wallets")->get(array("id"))->filter(array("id" =>$_GET["id_user"]))->send();
-				if(empty($array1[0][$_GET["id_user"]])){
+			$array1=Model::table("wallets")->get(array("id"))->filter(array("id" => $_GET["id_user"] ))->send();
+				if(empty($array1[0]["id"])){
 					$array1=Model::table("wallets")->add(array("id_user" => $_GET["id_user"], "amount" => $_GET["amount"],"image" => $_GET["image"]))->send();
-					var_dump($array1[0][$_GET["id_user"]]);
+					var_dump($array1[0]["id"]);
 				}
 					else{
 						echo('Ошибка');
