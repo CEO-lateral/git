6675c3a9d3e1485ccfc113a4a4eca9d719f04a48
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-30 08:15:07 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,1 +1,5 @@
-console.log("widget include");
+$(document).ready(function(){    
+   $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>');
+    $('#showInput').click(function(){
+        $('#inputs').show(600);        
+    });
