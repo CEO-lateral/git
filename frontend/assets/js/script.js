d7506fa03a2354f73b948908842ebf25ad03263c
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-30 08:17:10 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -3,3 +3,8 @@ $(document).ready(function(){
     $('#showInput').click(function(){
         $('#inputs').show(600);        
     });
+	$('#clear').click(function() {
+       $('.table').empty();
+       $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>');
+        console.log("clear");
+    });
