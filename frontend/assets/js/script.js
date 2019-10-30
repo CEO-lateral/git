bf321f68fd413cf9494c48ab57b8280366d62eb8
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-30 12:09:41 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
           } 
 		});
 		$('#add').click(function(){
+			console.log("data");
 		$.ajax({
 		url: 'https://k.qzo.su/api/wallets/addWallets', 
         type: 'GET',
