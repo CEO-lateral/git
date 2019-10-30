319a0da9b31ee02db27963bcadaac0c204e5ce14
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-30 23:55:11 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){    
-   $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>');
+   function (data){
+		var data = jQuery.parseJSON(JSON.stringify(data));
+		var html = '';
+        for (var i = 0; i < data.result.length; i++){
+        html += '<tbody><tr><td>' + data.result[i].id + '</td><td>' + data.result[i].id_user + '</td><td>' + data.result[i].amount + '</td><td>' + data.result[i].image + '</td></tr></tbody>';
+		}
+         $('.upd').append(html);				
+		} 
+		});
     $('#showInput').click(function(){
         $('#inputs').show(600);        
     });
@@ -8,6 +16,10 @@ $(document).ready(function(){
        $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>');
         console.log("clear");
     });
+		function update(){
+			$('.table').empty();
+			$('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>'); 
+}
 	//$('.table').view(function(){
 		$.ajax({
 		url: 'https://k.qzo.su/api/wallets/viewWallets', 
@@ -15,15 +27,6 @@ $(document).ready(function(){
         datatype: 'json',
         data: {},
         success:
-	function (data) {
-                        var data = jQuery.parseJSON(JSON.stringify(data));
-                        var html = '';
-                        for (var i = 0; i < data.result.length; i++) {
-                              html += '<tbody><tr><td>' + data.result[i].id + '</td><td>' + data.result[i].id_user + '</td><td>' + data.result[i].amount + '</td><td>' + data.result[i].image + '</td></tr></tbody>';
-                        }
-                        $('.upd').append(html);				
-          } 
-		});
 		/*$('#showInput').click(function(){
 			console.log("data222");
 $('.table').append('<thead><tr><th scope="col">13</th><th scope="col">13</th><th scope="col">1000</th><th scope="col">yes</th></tr></thead>');			
