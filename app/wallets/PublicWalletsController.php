a4f957254a4b7f7050358ec0ab8fe0401101b790
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-30 08:55:49 +0300

    fr

--- app/wallets/PublicWalletsController.php
+++ app/wallets/PublicWalletsController.php
@@ -43,7 +43,91 @@ class PublicWalletsController {
 	public function request1Wallets(){
 		$mywallet10=new Wallets();
 		$mywallet10->request1Wallets();
-	}	
+	}
+
+
+
+
+
+public function addUC(){ 
+        $class = new Users();
+        $class -> addUC();     
+    //level=3&user_type=user&image=1.jpg&nickname=nik&rating=2&description=text1
+    }
+    
+    public function addUPD(){ 
+        $class = new Users();
+        $class -> addUPD(); //password=12345&phone=893365563893&phone_token=54321&phone_token_data=543&doc_photo=photo&surname=surname&name=name&patronymic=patronymic&date_of_birth=000000&gender=male&other_data=texttext
+    
+    }
+    
+    public function apUPD(){ 
+        $class = new Users();
+        $class -> upUPD();         //password=54321&phone=13&phone_token=54321&phone_token_data=543&doc_photo=photo&surname=surname&name=name&patronymic=patronymic&date_of_birth=000000&gender=male&other_data=texttext&id=2
+    }
+    
+    public function upUC(){ 
+        $class = new Users();
+        $class -> upUC();         
+    //level=3&user_type=user&image=0&nickname=nik&rating=2&description=text1$id=1        
+    }
+    
+    
+    public function viewUC(){ 
+        $class = new Users();
+        $class -> viewUC();     
+    }
+    
+    public function viewUPD(){ 
+        $class = new Users();
+        $class -> viewUPD();     
+    }
+    
+    
+    public function viewUCId(){ 
+        $class = new Users();
+        $class -> viewUCSel();     
+    } 
+    
+    
+    public function insUPD(){ 
+        $class = new Users();
+        $class -> insUPD();
+    }
+    
+    public function insUC(){
+        $class = new Users();
+        $class -> insUC();
+        
+    }
+    
+    public function delUPD(){
+        $class = new Users();
+        $class -> delUPD();
+        
+    }
+    public function delUC(){
+        $class = new Users();
+        $class -> delUC();   
+    }
+    public function addUPDforUC(){
+        $class = new Users();
+        $class -> addUPDforUC();   
+    }
+    public function editUPDfree(){
+        $class = new Users();
+        $class -> editUPDfree();
+    }
+    public function viewUPDsortD(){
+        $class = new Users();
+        $class -> viewUPDsortD();
+    }
+    public function viewUPDsortA(){
+        $class = new Users();
+        $class -> viewUPDsortA();
+    }
+
+	
 }
 
 ?>
