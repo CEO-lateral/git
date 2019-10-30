0c77c77993656756c11f8ee21f44151f289fecec
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-30 08:19:58 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -8,3 +8,11 @@ $(document).ready(function(){
        $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>');
         console.log("clear");
     });
+	function table (data) {
+                        var data = jQuery.parseJSON(JSON.stringify(data));
+                        var html = '';
+                        for (var i = 0; i < data.result.length; i++) {
+                              html += '<tbody><tr><td>' + data.result[i].id + '</td><td>' + data.result[i].id_user + '</td><td>' + data.result[i].amount + '</td><td>' + data.result[i].image + '</td><td>'</tr></tbody>';
+                        }
+                        $('.upd').append(html);
+          } 
