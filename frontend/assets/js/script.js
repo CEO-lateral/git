60122dde6e81af81de1735162314be842e92ad61
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-31 13:25:21 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -16,8 +16,8 @@ $(document).ready(function(){
        console.log("clear");
     });
 		function update(){
-			$('.rap').empty();
-			//$('.rap').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>'); 
+			//$('.rap').empty();
+			$('.rap').append('<thead><tr><th scope="col">id</th><th scope="col">id_user</th><th scope="col">amount</th><th scope="col">image</th></tr></thead>'); 
 		}
 	function view(){
 		$.ajax({
