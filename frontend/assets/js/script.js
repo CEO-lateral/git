06ebf781a3ec08cb225e36d7e649babb2c26ccff
Konstantin Nevedomskiy <august1403@mail.ru>
2019-10-31 13:17:53 +0300

    fr

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -45,8 +45,8 @@ $(document).ready(function(){
         data: {},
         success: 
 			function(data){
+                    update();                   
                    
-                    rap(data);
             }
 		});
 	});
